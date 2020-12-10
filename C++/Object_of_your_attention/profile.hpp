a99e3734c79950cb2c3ce65837d2156edebaa845
DonnaDia <0dvd0bvb0@gmail.com>
2020-12-10 19:34:39 +0200

    .view_profile added

--- C++/Object_of_your_attention/profile.hpp
+++ C++/Object_of_your_attention/profile.hpp
@@ -1,6 +1,7 @@
 #include <vector>
 
 class Profile {
+
 private: 
   std::string name;
   int age;
@@ -8,6 +9,11 @@ private:
   std::string country;
   std::string pronouns;
   std::vector<std::string> hobbies;
+  
 public: 
   Profile(std::string new_name, int new_age, std::string new_city, std::string new_country, std::string new_pronouns = "they/them");
+  
+  std::string view_profile();
+
+  void add_hobby(std::string new_hobby);
 };
