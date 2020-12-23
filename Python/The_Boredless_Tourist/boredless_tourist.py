39957e5773bb62676d16784d4b6c3dfd9cebd15c
DonnaDia <0dvd0bvb0@gmail.com>
2020-12-23 11:22:18 +0200

    final version

--- Python/The_Boredless_Tourist/boredless_tourist.py
+++ Python/The_Boredless_Tourist/boredless_tourist.py
@@ -1,3 +1,4 @@
+# Setting up the project
 destinations = ["Paris, France", 
                 "Shanghai, China",
                 "Los Angeles, USA",
@@ -6,6 +7,7 @@ destinations = ["Paris, France",
 
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
 
+# Travelling to Faraway Lands
 def get_destination_index(destination):
   destination_index = destinations.index(destination)
   return destination_index
@@ -15,6 +17,7 @@ def get_traveler_location(traveler):
   traveler_destination_index = get_destination_index(traveler_destination)
   return traveler_destination_index
 
+# Visiting interesting placess
 attractions = [[] for destination in destinations]
 
 def add_attraction(destination, attraction):
@@ -26,7 +29,7 @@ def add_attraction(destination, attraction):
     attractions_for_destination.append(attraction)
     return
   
-
+# Adding attractions for different destinations
 add_attraction("Los Angeles, USA", ['Venice Beach', ['beach']])
 add_attraction("Paris, France", ["the Louvre", ["art", "museum"]])
 add_attraction("Paris, France", ["Arc de Triomphe", ["historical site", "monument"]])
@@ -39,14 +42,38 @@ add_attraction("São Paulo, Brazil", ["Pátio do Colégio", ["historical site"]]
 add_attraction("Cairo, Egypt", ["Pyramids of Giza", ["monument", "historical site"]])
 add_attraction("Cairo, Egypt", ["Egyptian Museum", ["museum"]])
 
-#print(attractions)
-
-
+# Finding the best places to go
 def find_attractions(destination, interests):
   destination_index = get_destination_index(destination)
+  attractions_with_interest = []
   attractions_in_city = attractions[destination_index]
+  attractions_with_interest = []
+  for attraction in attractions_in_city:
+    possible_attraction = attraction
+    attraction_tags = attraction[1]
+
+    for interest in interests:
+      if interest in attraction_tags:
+        attractions_with_interest.append(possible_attraction[0])
+
+  return attractions_with_interest
+
+# Show the attractions according to interests
+def get_attractions_for_traveler(traveler):
+  traveler_destination = traveler[1]
+  traveler_interests = traveler[2]
+  traveler_attractions = find_attractions(traveler_destination, traveler_interests)
   
+  interests_string = "Hi " + traveler[0] + ", we think you'll like these places around " + traveler_destination + ":"
+  for attraction in traveler_attractions:
+    if len(traveler_attractions) > 1:
+      interests_string += " the " + attraction + ', '
+    else:
+      interests_string += " the " + attraction + "."
 
+  
+  return interests_string
 
-
-
+# Test 
+smills_france = get_attractions_for_traveler(['Dereck Smill', 'Paris, France', ['monument']])
+print(smills_france)
