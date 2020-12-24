154195c3ab4d892c8aaf75fd0f16d42e400929f7
DonnaDia <0dvd0bvb0@gmail.com>
2020-12-24 19:55:40 +0200

    numpy project with a histogram!

---
+++ Python/Numpy/election_results.py
@@ -0,0 +1,34 @@
+import numpy as np
+from matplotlib import pyplot as plt
+
+survey_responses = ['Ceballos', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos','Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos',
+'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Ceballos', 'Ceballos', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Kerrigan', 'Ceballos']
+
+total_ceballos = sum([1 for i in survey_responses if i == 'Ceballos'])
+
+survey_length = float(len(survey_responses))
+
+percentage_ceballos = 100 * total_ceballos / survey_length
+
+
+print(percentage_ceballos)
+
+
+possible_surveys = np.random.binomial(survey_length, 0.54, 10000) / survey_length
+
+plt.hist(possible_surveys, range=(0, 1), bins=20)
+
+plt.show()
+
+ceballos_loss_surveys = np.mean(possible_surveys < 0.5)
+
+print(ceballos_loss_surveys)
+
+large_survey =np.random.binomial(float(7000), 0.54, 10000) / survey_length
+
+ceballos_loss_new = np.mean(large_survey < 0.5)
+print(ceballos_loss_new)
+
+
