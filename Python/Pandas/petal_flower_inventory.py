a7d34273b133af645547dfdf884dfbf4a541160f
DonnaDia <0dvd0bvb0@gmail.com>
2020-12-26 16:21:23 +0200

    final version

--- Python/Pandas/petal_flower_inventory.py
+++ Python/Pandas/petal_flower_inventory.py
@@ -9,4 +9,17 @@ product_request = staten_island.product_description
 
 seed_request = inventory[(inventory.location == 'Brooklyn') & (inventory.product_type == 'seeds')]
 
-print(seed_request)
+
+inventory['in_stock'] = inventory.apply(lambda row: True if row.quantity > 0 else False, axis=1)
+
+inventory["total_value"] = inventory.price * inventory.quantity
+
+combine_lambda = lambda row: \
+    '{} - {}'.format(row.product_type,
+                     row.product_description)
+
+inventory["full_description"] = inventory.apply(combine_lambda, axis = 1)
+
+print(inventory)
+
+
