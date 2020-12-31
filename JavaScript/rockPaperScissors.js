bce67438f1ac1a8549ca20aafc175f3855aadf2e
DonnaDia <0dvd0bvb0@gmail.com>
2020-12-31 23:10:08 +0200

    full version

--- JavaScript/rockPaperScissors.js
+++ JavaScript/rockPaperScissors.js
@@ -19,3 +19,40 @@ function getComputerChoice() {
   }
 };
 
+function determineWinner(userChoice, computerChoice) {
+  if (userChoice === computerChoice) {
+    return "That's a tie!";
+  } else if (userChoice === 'rock') {
+    if (computerChoice === 'paper') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  } else if (userChoice === 'paper') {
+    if (computerChoice === 'scissors') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  } else if (userChoice === 'scissors') {
+    if (computerChoice === 'rock') {
+      return 'The computer won!';
+    } else {
+      return 'You won!';
+    }
+  } else if (userChoice === 'bomb') {
+    return 'You won!';
+  }
+  
+}
+
+const playGame = () => {
+  const userChoice = getUserChoice('scissors');
+  const computerChoice = getComputerChoice();
+  console.log('You threw: ' + userChoice);
+  console.log('The computer threw: ' + computerChoice);
+  console.log(determineWinner(userChoice, computerChoice));
+};
+
+playGame();
+
