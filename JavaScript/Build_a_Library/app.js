9965fd7898899e2880bf41a16dbdcd3715aecffc
DonnaDia <0dvd0bvb0@gmail.com>
2021-01-11 17:53:40 +0200

    created a parent class

---
+++ JavaScript/Build_a_Library/app.js
@@ -0,0 +1,36 @@
+class Media{
+  constructor(title){
+    this._title = title;
+    this._isCheckedOut = false;
+    this._ratings = [];
+  }
+
+  get isCheckedOut(){
+    return this._isCheckedOut;
+  }
+
+  get ratings(){
+    return this._ratings;
+  }
+
+  set isCheckedOut(newValue){
+    this._isCheckedOut = newValue;
+  }
+
+  toggleCheckOutStatus(){
+    if (this._isCheckedOut === true){
+      this._isCheckedOut = false;
+    }else{
+      this._isCheckedOut = true;
+    }
+  }
+
+  getAverageRating(){
+    let ratingsSum = this.ratings.reduce((currentSum, rating) => currentSum + rating, 0)/this._ratings.length;
+     return ratingsSum;
+  }
+
+  addRating(newRating){
+    this._ratings.push(newRating);
+  }
+}
