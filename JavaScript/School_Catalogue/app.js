491ce45a2f2faf03fc30db15fab769b3a8f0dc49
DonnaDia <0dvd0bvb0@gmail.com>
2021-01-11 17:20:01 +0200

    declared the main class

---
+++ JavaScript/School_Catalogue/app.js
@@ -0,0 +1,36 @@
+class School{
+  constructor(name, level, numberOfStudents){
+    this._name = name;
+    this._level = level;
+    this._numberOfStudents = numberOfStudents;
+  }
+
+  get name(){
+    return this._name;
+  }
+
+  get level(){
+    return this._level;
+  }
+
+  get numberOfStudents(){
+    return this._numberOfStudents;
+  }
+
+  set numberOfStudents(newNumberOfStudents){
+    if (typeof newNumberOfStudents === number){
+      this._numberOfStudents = newNumberOfStudents;
+    }else{
+      console.log('Invalid input: numberOfStudents must be set to a Number.');
+    }
+  }
+
+  quickFacts(){
+    console.log(`${this._name} educates ${this._numberOfStudents} students at the ${this._level} school level.`)
+  }
+
+  static pickSubstituteTeacher(substituteTeachers){
+    return substituteTeachers[Math.floor(Math.random() * substituteTeachers.length)]
+  }
+}
+
