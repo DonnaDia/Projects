5374c477df0d812b1756554b674416b11e34b88c
DonnaDia <0dvd0bvb0@gmail.com>
2021-01-11 18:06:51 +0200

    created a Movie class. Tested everything.

--- JavaScript/Build_a_Library/app.js
+++ JavaScript/Build_a_Library/app.js
@@ -50,3 +50,44 @@ class Book extends Media{
     return this._pages;
   }
 }
+
+class Movie extends Media{
+  constructor(director, title, runTime){
+    super(title);
+    this._director = director;
+    this._runTime = runTime;
+  }
+
+  get director(){
+    return this._director;
+  }
+
+  get runTime(){
+    return this._runTime;
+  }
+}
+// test of a Book class
+const historyOfEverything = new Book('Bill Bryson', 'A Short History of Nearly Everything', 544);
+
+historyOfEverything.toggleCheckOutStatus();
+
+console.log(historyOfEverything.isCheckedOut);
+
+historyOfEverything.addRating(4);
+historyOfEverything.addRating(5);
+historyOfEverything.addRating(5);
+
+console.log(historyOfEverything.getAverageRating());
+
+
+// test of a Movie class
+const speed = new Movie('Jan de Bont', 'Speed', 116);
+
+speed.toggleCheckOutStatus();
+console.log(speed.isCheckedOut);
+
+speed.addRating(1);
+speed.addRating(1);
+speed.addRating(5);
+
+console.log(speed.getAverageRating());
