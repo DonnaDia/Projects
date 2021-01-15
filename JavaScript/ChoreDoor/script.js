656dd31e917b90f97de01f29158275297a3f68cc
DonnaDia <0dvd0bvb0@gmail.com>
2021-01-15 17:16:19 +0200

    initial commit

---
+++ JavaScript/ChoreDoor/script.js
@@ -0,0 +1,6 @@
+let doorImage1 = document.getElementbyId('door1');
+const botDoorPath = "https://s3.amazonaws.com/codecademy-content/projects/chore-door/images/robot.svg";
+
+door1.onclick = () => {
+doorImage1.src=botDoorPath
+}
