e6ff00fe2ed118a90581945000962366c37aff62
DonnaDia <0dvd0bvb0@gmail.com>
2021-01-25 18:41:13 +0200

    sololearn projects in js

---
+++ JavaScript/theSnailInTheWell.js
@@ -0,0 +1,11 @@
+function main() {
+    var depth = parseInt(readLine(), 10);
+    var i;
+    var days;
+    for(i = 0; i < depth; i++){
+        i += 5;
+        
+    }
+    console.log(Math.round(i / 5));
+}
+
