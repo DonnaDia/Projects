559abeb982eac853555782c581c743df4a7fd34a
DonnaDia <0dvd0bvb0@gmail.com>
2021-02-18 15:32:43 +0200

    refactored code to use functions

--- Python/Medical_Insurance_Project/script.py
+++ Python/Medical_Insurance_Project/script.py
@@ -1,33 +1,11 @@
-age = 28;
-sex = 0;
-bmi = 26.2;
-num_of_children = 3;
-smoker = 0;
+# Create calculate_insurance_cost() function below: 
+def calculate_insurance_cost(age, sex, bmi, num_of_children, smoker, name):
+  estimated_cost = 250*age - 128*sex + 370*bmi + 425*num_of_children + 24000*smoker - 12500
+  print(f'The estimated insurance cost for {name} is {estimated_cost} dollars.')
+  return estimated_cost
 
-insurance_cost = (250 * age) - (128 * sex) + (370 * bmi) + (425 * num_of_children) + (24000 * smoker) - 12500
+# Estimate Maria's insurance cost
+insurance_cost = calculate_insurance_cost(28, 0, 26.2, 3, 0, 'Maria')
 
-print(f"This person's insurance cost is {insurance_cost} dollars.")
-
-# Age Factor
-age += 4;
-new_insurance_cost = (250 * age) - (128 * sex) + (370 * bmi) + (425 * num_of_children) + (24000 * smoker) - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-print(f"The change in cost of insurance after increasing the age by 4 years is {new_insurance_cost} dollars.")
-# BMI Factor
-age -= 4 #getting age back
-bmi += 3.1
-
-new_insurance_cost = (250 * age) - (128 * sex) + (370 * bmi) + (425 * num_of_children) + (24000 * smoker) - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print(f"The change in estimated insurance cost after increasing BMI by 3.1 is {change_in_insurance_cost} dollars.")
-# Male vs. Female Factor
-bmi -= 3.1 #getting bmi back
-sex = 1
-new_insurance_cost = (250 * age) - (128 * sex) + (370 * bmi) + (425 * num_of_children) + (24000 * smoker) - 12500
-
-change_in_insurance_cost = new_insurance_cost - insurance_cost
-
-print(f"The change in estimated cost for being male instead of female is {change_in_insurance_cost} dollars.")
+# Estimate Omar's insurance cost 
+insurance_cost = calculate_insurance_cost(35, 1, 22.2, 0, 1, 'Omar')
