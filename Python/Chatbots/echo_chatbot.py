37290bc4923a226bf5d682615924069fad71e315
DonnaDia <0dvd0bvb0@gmail.com>
2021-03-09 15:54:41 +0200

    initial commit

---
+++ Python/Chatbots/echo_chatbot.py
@@ -0,0 +1,9 @@
+print('Hi, I am a chatbot!')
+print("I will repeat after you.")
+
+
+stuff_to_echo = input("Enter something for me to repeat: ")
+
+if stuff_to_echo:
+  print("You said: " + stuff_to_echo)
+
